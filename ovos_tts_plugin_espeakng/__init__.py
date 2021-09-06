cb465cceeff4bd09a13334bfe98db2c56565fb7e
Joan Palacios <jopabel93@gmail.com>
2021-09-06 08:26:58 -0400

    Added lang as an optional parameter to get_tts (#1)

--- ovos_tts_plugin_espeakng/__init__.py
+++ ovos_tts_plugin_espeakng/__init__.py
@@ -33,9 +33,10 @@ class EspeakNGTTS(TTS):
                 tag = tag.replace(val, new_val)
         return tag
 
-    def get_tts(self, sentence, wav_file):
+    def get_tts(self, sentence, wav_file, lang=None):
+        lang = lang or self.lang
         subprocess.call(
-            ['espeak-ng', '-m', "-w", wav_file, '-v', self.lang + '+' +
+            ['espeak-ng', '-m', "-w", wav_file, '-v', lang + '+' +
              self.voice, sentence])
         return wav_file, None
 
